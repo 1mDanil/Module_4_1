ec771dadf7a79fbdb25354be895d806defdfa1f2
1mDanil <danya7540@gmail.com>
2024-11-19 15:17:31 +0300

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as broke
+from True_math import divide as fixed
+
+broke(18, 6)
+broke(4, 0)
+fixed(800, 10)
+fixed(10, 0)
---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(a, b):
+    if b != 0:
+        c = a / b
+        print(c)
+    else:
+        print('Делить на 0 нельзя')
